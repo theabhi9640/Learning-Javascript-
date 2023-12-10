9f73dcace6e79561cc4aa074f852cd6f1405f140
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2023-12-10 08:13:48 +0000

    01_Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1445
+let accountEmail = "yadavabhishek6660@gmail.com"
+var accountPassword = "abhi1234"
+accountCity = "Etawah"
+
+//accountId = 2 not allowed
+
+accountEmail = "abc@123.com"
+accountCity = "Ghaziabad"
+accountPassword = ""
+
+console.log(accountId);
