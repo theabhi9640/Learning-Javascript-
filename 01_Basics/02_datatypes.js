5975e01d2e03adfdacf526c21bb732be5947fcb2
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2024-01-28 22:19:52 +0000

    Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3 + 3) we aree using node js, not browser
+
+console.log(3 
+    +
+    3); // code readability should be high
+
+    console.log("Abhishek")
+
+    let name = "Abhishek" //string datatype
+    let age = 18 // number datatype
+    let isLoggedIn = false // boolean data type
+
+// datatypes primitive
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => now value not assign 
+// symbol => unique
+
+// datatypes non-primitive
+
+// object
+
+console.log(typeof undefined);  // undefined
+consloe.log(typeof null); //object
+
