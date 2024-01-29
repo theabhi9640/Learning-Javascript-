b28b8131be4dc54336e2af8ce2f484f7526c8209
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2024-01-29 18:08:11 +0000

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -24,7 +24,25 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "abhishek" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber); 
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber); 
+// console.log(typeof stringNumber);
+
+
+// *********************************operations***********************
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " abhishek"
+
+let str3 = str1 + str2
+console.log (str3);
