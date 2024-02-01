c10cace0a977ec206ef540f23e584d369022f730
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2024-02-01 08:44:50 +0000

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -45,4 +45,23 @@ let str1 = "hello"
 let str2 = " abhishek"
 
 let str3 = str1 + str2
-console.log (str3);
+// console.log (str3);
+
+// console.log("1" + 2);  //12
+// console.log(1 + "2");  //12
+// console.log("1" + 2 + 2);  //122
+// console.log( 1 + 2 + "2"); // 32 
+
+// console.log( +true ); // 1
+// console.log(+ "");  //0
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); // 101
+
+// study about prefix and postfix on md3
+// study about operation on ECMA 
+
