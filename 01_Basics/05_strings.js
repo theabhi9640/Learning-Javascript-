130fb93b39adcfb6a844048effaa6dab5c1c652f
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2024-02-03 18:00:55 +0000

    strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,2 +1,42 @@
-let str1 = 'Abhihek';
-console.log(str1.length)
+const name = "Abhishek";
+// const repoCount = 20
+const sname = "Yadav";
+
+
+//console.log(name ,sname)
+// console.log(name + repoCount + "Value"); 
+
+//console.log("Hello my name is "+ name , "and my sirname "+ sname);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('abhishek-hc');
+
+/*console.log(gameName[0]); // key 1 (a)
+console.log(gameName.__proto__); // prototype ({}) 
+console.log(gameName.length);
+console.log(gameName.toUpperCase()); */
+
+/*console.log(gameName.charAt(2));  // h
+console.log(gameName.indexOf('h')); // 2 */
+
+/*const newString = gameName.substring(0,4);
+console.log(newString);  // abhi
+
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString); */
+
+const newStringOne = "   abhishek    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https;//hitesh.com/hitesh%20chaudhary"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('hitesh'))  // true
+console.log (url.includes('sundar')) // false 
+
+// console.log(gameName.split('-'));
